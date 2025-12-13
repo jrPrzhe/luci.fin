486ca2a01eecf42793eaf972645e66592c11f49b
jrPrzhe <przheradskiy@gmail.com>
2025-12-13 11:48:01 +0800

    Fix migration: correct down_revision to point to 4a382c2ee966

--- backend/alembic/versions/b39c32f206f8_add_theme_to_users.py
+++ backend/alembic/versions/b39c32f206f8_add_theme_to_users.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'b39c32f206f8'
-down_revision = 'cdbfcee9a49f'
+down_revision = '4a382c2ee966'
 branch_labels = None
 depends_on = None
 
